b1cbc9b4a40881bef368b9918248cef139ae7b9b
dangrabo <193651636+dangrabo@users.noreply.github.com>
2026-01-13 14:37:54 -0800

    fixed route typos in player routes, implemented game routes

--- backEnd/src/routes/PlayerRoutes.js
+++ backEnd/src/routes/PlayerRoutes.js
@@ -10,9 +10,9 @@ import {
 const PlayerRouter = Router();
 
 PlayerRouter.get("/players", getAllPlayers);
-PlayerRouter.get("players/:id", getPlayer);
+PlayerRouter.get("/players/:id", getPlayer);
 PlayerRouter.post("/players", createPlayer);
-PlayerRouter.put("players/:id", editPlayer);
+PlayerRouter.put("/players/:id", editPlayer);
 PlayerRouter.delete("/players/:id", deletePlayer);
 
 export default PlayerRouter;
