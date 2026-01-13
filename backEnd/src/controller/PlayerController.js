a756b944553d8de3803ea5f09ce9a1a9d758dab3
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2026-01-13 14:38:13 -0800

    Add createPlayer function

--- backEnd/src/controller/PlayerController.js
+++ backEnd/src/controller/PlayerController.js
@@ -18,3 +18,22 @@ export const getPlayer = async (req, res) => {
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const createPlayer = async (req, res) => {
+  try {
+    const { first_name, last_name } = req.body;
+    if (!first_name || !last_name)
+      return res.status(400).json({ message: "Missing required fields" });
+
+    const player = await Player.create({ first_name, last_name });
+
+    await Stats.create({
+      player_id: player.id,
+    });
+
+    return res.status(201).json({ message: "Player Created" });
+  } catch (error) {
+    console.error("Error creating player:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
