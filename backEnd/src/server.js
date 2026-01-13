8fc65ea16490abdf0230e3e6f24cd6b01afb17c8
dangrabo <193651636+dangrabo@users.noreply.github.com>
2026-01-13 14:15:17 -0800

    Db and server setup setup (#4)
    * created new branch and starter file for db connection
    
    * Add initial server setup
    
    * Add player schema in models folder
    
    * pushing db files for alex to test
    
    * implemented round schema, cleaned db connection
    
    * Add Game schema for game creation
    
    * Add Stats schema for stats table creation
    
    * implemented PlayerRoundSchema
    
    * Add DB diagram
    
    * implemented PlayerGameSchema
    
    * Add linking between tables
    
    * Add db config into server.js so that the tables are created and linked when ran
    
    * Add sever script
    
    * Add nodemon and proper script to run
    
    * troubleshooting / save progress
    
    ---------
    
    Co-authored-by: BolshiAlex <145608606+Bolshialex@users.noreply.github.com>

---
+++ backEnd/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import cors from "cors";
+import db from "./models/index.js";
+
+dotenv.config();
+const PORT = 3056;
+const app = express();
+app.use(express());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+await db.sequelize.sync();
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`.bgYellow);
+});
