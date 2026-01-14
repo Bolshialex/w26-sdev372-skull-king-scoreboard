1916cc03ef298aa1ea28cd56f7844ebd6f232e1f
BolshiAlex <145608606+Bolshialex@users.noreply.github.com>
2026-01-14 15:55:42 -0800

    Add components and home form

---
+++ frontEnd/src/components/HomeForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HomeForm() {
+  return <div>home form goes here</div>;
+}
+
+export default HomeForm;
--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import HomeForm from "../components/HomeForm";
 
 function Home() {
   return (
     <div>
       <p>This is Home</p>
+      <HomeForm />
     </div>
   );
 }
