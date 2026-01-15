ae066783bced2389dd8169a20b407978f3036785
BolshiAlex <145608606+Bolshialex@users.noreply.github.com>
2026-01-14 17:34:38 -0800

    Add player creation page and route in main

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import PlayerCreation from "./pages/PlayerCreation";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/player/create" element={<PlayerCreation />} />
         </Routes>
       </Router>
     </>
---
+++ frontEnd/src/pages/PlayerCreation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function PlayerCreation() {
+  return (
+    <div>
+      <div>
+        <h1>Player Creation</h1>
+        <p>Insert Player Info</p>
+      </div>
+      <div>{/* Component goes here */}</div>
+    </div>
+  );
+}
+
+export default PlayerCreation;
