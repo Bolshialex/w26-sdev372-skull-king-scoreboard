24e923f5582d6fc7890692ef1a5eb761db8daade
BolshiAlex <145608606+Bolshialex@users.noreply.github.com>
2026-01-19 22:12:45 -0800

    Add GameCreation page

---
+++ frontEnd/src/components/GameCreationForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GameCreationForm() {
+  return <div></div>;
+}
+
+export default GameCreationForm;
---
+++ frontEnd/src/pages/GameCreation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function GameCreation() {
+  return (
+    <div>
+      <div>
+        <h1>Start A New Game</h1>
+      </div>
+      <div>
+        <p>Game Settings</p>
+        {/* Game Form goes here */}
+      </div>
+    </div>
+  );
+}
+
+export default GameCreation;
