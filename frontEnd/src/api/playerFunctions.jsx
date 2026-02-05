3f05bb892a0e6ac2c4530e425dfb104f55f60f2f
dangrabo <193651636+dangrabo@users.noreply.github.com>
2026-02-04 16:29:31 -0800

    updated API_URL to point to deployed backend

--- frontEnd/src/api/playerFunctions.jsx
+++ frontEnd/src/api/playerFunctions.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:3056";
+const API_URL = "http://209.38.154.66:3056";
 
 async function createPlayer(playerInfo) {
   const { first_name, last_name } = playerInfo;
